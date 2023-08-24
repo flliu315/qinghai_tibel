89b353cafcc031415a15a7be824b404b2b322df4
flliu315 <flliu315@163.com>
2023-08-24 16:22:33 +0800

    added

--- Rscript/landscape_analysis.R
+++ Rscript/landscape_analysis.R
@@ -1,5 +1,31 @@
-library(motif)
+# source: https://github.com/Nowosad/motif-examples
+
+rm(list = ls())
+
+# install packages
+
+# pkgs = c("stars", "sf", "tmap",
+#          "dplyr", "readr", "osfr",
+#          "spdep", "motif")
+# remotes::install_cran(pkgs)
+
+# get data and load them into R
+library(osfr)
+# dir.create("data")
+# osf_retrieve_node("xykzv") %>%
+#   osf_ls_files(n_max = Inf) %>%
+#   osf_download(path = "data",
+#                conflicts = "overwrite")
+
 library(stars)
+landcover <- read_stars("data/land_cover.tif") # load lc
+# land cover composition was derived based on regular non-overlapping regions
+comp_out <- lsp_signature(landcover,
+                          type = "composition", # proportion of each land cover class
+                          window = 200) # 200 by 200 cells
+
+library(motif)
+
 library(sf)
 safe_pal6 = c("#88CCEE", "#CC6677", "#DDCC77",
               "#117733", "#332288", "#888888")
